09d80ec30cd99228d952ff4afc654a7e5010ddf3
wechta <wechta@gmail.com>
2016-01-18 20:28:15 +0100

    - popravek tabov - popravke glavnega menija - dodal API klice

---
+++ api-sample/core.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+
+function mt_api_call($action = "", $params = array())
+	{	
+	//api url
+	$api_url = "http://dev.minitron-apps.si/api/";
+	
+	//session
+	$api_sess = &$_SESSION['mt_api_sess'];
+	
+	//get request params
+	$lnk = "";
+	$lnk .= "&mod=api";
+	if ($action) {$lnk .= "&action=" . $action;}
+	if ($api_sess) {$lnk .= "&sess=" . $api_sess;}
+	$lnk .= "&mode=json";
+	$lnk = substr($lnk, 1);
+	
+	//post request params	
+	$post_request = "";
+	if (is_array($params))
+		{foreach(array_keys($params) as $key)
+			{$post_request .= "&" . $key . "=" . urlencode($params[$key]);}
+		}
+	//var_dump ($post_request);
+	//prevent cache
+	$api_url .= "?" . $lnk . "&f=" . time();
+	//echo $api_url;
+	//curl init
+	$ch = curl_init($api_url); 
+	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch,CURLOPT_POST,true);
+	curl_setopt($ch,CURLOPT_POSTFIELDS,$post_request);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+	curl_setopt($ch,CURLOPT_TIMEOUT,30);
+	
+	$response = curl_exec($ch);
+	//echo $response;
+	$response_parsed = json_decode($response);
+	$response_parsed = mt_api_stdclass2array($response_parsed);
+		
+	if (curl_errno($ch) || !$response)
+		{$response_parsed["status"] = 0;}
+	
+	//session
+	if (!$api_sess) {$api_sess = $response_parsed["sess"];}
+	
+	curl_close($ch);
+	
+	return $response_parsed;
+	}
+
+function mt_api_stdclass2array($std_object) 
+	{
+	if (is_object($std_object)) 
+		{$std_object = get_object_vars($std_object);}
+	
+	if (is_array($std_object))
+		{return array_map(__FUNCTION__, $std_object);}
+	else 
+		{return $std_object;}
+	}
+?>
---
+++ api-sample/get_articles.php
@@ -0,0 +1,48 @@
+<?php
+include_once("core.php");
+
+//autheticate
+$api_sess = &$_SESSION['mt_api_sess'];
+$api_user = "api.abubushop";
+$api_hash = "i397c6g4e1dc2d22d025b291cd0b2b3a9dLb2buu";
+
+$result = mt_api_call("init", array("api_user" => $api_user, "api_hash" => $api_hash));
+
+//dobis vse kategorije, ce ima pid=0 pol je glavna, upostevas vse active=1, razen Blagajna 01
+/*$result = mt_api_call("getCategories", array("all" => "1"));
+echo "<pre>";
+var_dump($result);
+echo "</pre>";*/
+
+//dobis vse propertije
+/*$result = mt_api_call("getProps", array("all" => "1"));
+echo "<pre>";
+var_dump($result);
+echo "</pre>";*/
+
+//dobis vse GLAVNE izdelke.. zraven imas kateri kategoriji spadajo
+/*echo "<pre>";
+$i = 0;
+$result = mt_api_call("getArticles", array("all" => "1"));
+foreach ($result['data'] as $single) {
+  if($single['pid']==0 && $single['web']==1 && $single['active']==1) {
+    $i++;
+    var_dump($single);
+  }
+}
+var_dump('SKUPAJ GLAVNIH IZDELKOV: '.$i);
+echo "</pre>";*/
+
+
+//dobis podrejene izdelke za posamezen glavni izdelek, te potem zdruzis v en izdelek..
+/*echo "<pre>";
+$result = mt_api_call("getLinkedArt", array("id" => "2"));
+foreach ($result['data'] as $single) {
+  if($single['pid'] != 0) {
+    $result2 = mt_api_call("getArticles", array("id" => $single['art']));
+    var_dump($result2['data']);
+  }
+}
+echo "</pre>";*/
+
+?>
